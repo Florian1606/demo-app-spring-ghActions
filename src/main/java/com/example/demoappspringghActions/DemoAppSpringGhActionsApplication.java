88e5df8e45ccbdb5eb55a46c0a06aa3661de16d3
69862920+Florian1606 <69862920+Florian1606@users.noreply.github.com>
2023-01-09 13:26:51 +0100

    init spring project

---
+++ src/main/java/com/example/demoappspringghActions/DemoAppSpringGhActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoappspringghActions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppSpringGhActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppSpringGhActionsApplication.class, args);
+	}
+
+}
